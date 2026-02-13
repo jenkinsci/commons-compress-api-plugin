f4d296ab226994aed6e2a84b5d9cd0ba42276849
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-02-13 13:38:56 +0100

    Add more compress/uncompress tests for 3 formats

--- src/test/java/io/jenkins/plugins/compress/api/SmokeTests.java
+++ src/test/java/io/jenkins/plugins/compress/api/SmokeTests.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.apache.commons.compress.compressors.xz.XZCompressorOutputStream;
 import org.apache.commons.compress.compressors.zstandard.ZstdCompressorOutputStream;
 import org.junit.jupiter.api.Disabled;
@@ -28,31 +29,57 @@ class SmokeTests {
     @TempDir(cleanup = CleanupMode.ON_SUCCESS)
     private static Path targetDir;
 
-    /**
-     * Decompress existing test archives
-     * @param file the archive file name
-     * @throws Exception if an error occurs during decompression
-     */
     @ParameterizedTest
     @ValueSource(strings = {"jenkins-chart.tgz", "jenkins-sources.tar.gz"})
-    @Disabled("Fixed on 1.29.0 https://issues.apache.org/jira/browse/COMPRESS-705")
-    void uncompressArchives(String file) throws Exception {
+    @Disabled("Fixed on commons-compress 1.29.0 https://issues.apache.org/jira/browse/COMPRESS-705")
+    void uncompressGzip(String file) throws Exception {
         Path archive = Paths.get("src/test/resources").resolve(file);
         uncompressGzip(Files.newInputStream(archive));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"jenkins-chart.tgz", "jenkins-sources.tar.gz"})
+    @ValueSource(strings = {"jenkins-chart.tar.zst", "jenkins-sources.tar.zst"})
+    void uncompressZSTD(String file) throws Exception {
+        Path archive = Paths.get("src/test/resources").resolve(file);
+        uncompressZSTD(Files.newInputStream(archive));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"jenkins-chart.tar.xz", "jenkins-sources.tar.xz"})
+    void uncompressXZ(String file) throws Exception {
+        Path archive = Paths.get("src/test/resources").resolve(file);
+        uncompressXZ(Files.newInputStream(archive));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"jenkins-chart.tar", "jenkins-sources.tar"})
+    void compressGzip(String file) throws Exception {
+        Path archive = Paths.get("src/test/resources").resolve(file);
+        compressGzip(archive, targetDir.resolve("%s.gz".formatted(file)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"jenkins-chart.tar", "jenkins-sources.tar"})
     void compressZSTD(String file) throws Exception {
         Path archive = Paths.get("src/test/resources").resolve(file);
         compressZSTD(archive, targetDir.resolve("%s.xz".formatted(file)));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"jenkins-chart.tgz", "jenkins-sources.tar.gz"})
+    @ValueSource(strings = {"jenkins-chart.tar", "jenkins-sources.tar"})
     void compressXZ(String file) throws Exception {
         Path archive = Paths.get("src/test/resources").resolve(file);
         compressXZ(archive, targetDir.resolve("%s.xz".formatted(file)));
+    }
+
+    private void compressGzip(Path file, Path target) throws IOException {
+        try (InputStream fis = Files.newInputStream(file);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                OutputStream fos = Files.newOutputStream(target);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                GzipCompressorOutputStream gzos = new GzipCompressorOutputStream(bos)) {
+            bis.transferTo(gzos);
+        }
     }
 
     private void compressZSTD(Path file, Path target) throws IOException {
@@ -75,7 +102,7 @@ class SmokeTests {
         }
     }
 
-    private static void uncompressGzip(InputStream inputStream) throws IOException {
+    private void uncompressGzip(InputStream inputStream) throws IOException {
         Path tarFile = Files.createTempFile("jenkins", "temporary");
         try (BufferedInputStream bis = new BufferedInputStream(inputStream);
                 GzipCompressorInputStream gzis = new GzipCompressorInputStream(bis);
@@ -83,6 +110,22 @@ class SmokeTests {
                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
 
             gzis.transferTo(bos);
+        }
+    }
+
+    private void uncompressZSTD(InputStream inputStream) throws IOException {
+        Path tarFile = Files.createTempFile("jenkins", "temporary");
+        try (BufferedInputStream bis = new BufferedInputStream(inputStream);
+                ZstdCompressorOutputStream zstdos = new ZstdCompressorOutputStream(Files.newOutputStream(tarFile))) {
+            bis.transferTo(zstdos);
+        }
+    }
+
+    private void uncompressXZ(InputStream inputStream) throws IOException {
+        Path tarFile = Files.createTempFile("jenkins", "temporary");
+        try (BufferedInputStream bis = new BufferedInputStream(inputStream);
+                XZCompressorOutputStream zstdos = new XZCompressorOutputStream(Files.newOutputStream(tarFile))) {
+            bis.transferTo(zstdos);
         }
     }
 }
